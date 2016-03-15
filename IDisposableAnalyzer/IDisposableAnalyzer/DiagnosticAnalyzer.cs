916114a7751b1d2e15746986ad18593bea0b27c9
medmondson <eddy556@gmail.com>
2016-03-15 00:14:36 +0000

    Periodic checkin

--- IDisposableAnalyzer/IDisposableAnalyzer/DiagnosticAnalyzer.cs
+++ IDisposableAnalyzer/IDisposableAnalyzer/DiagnosticAnalyzer.cs
@@ -2,6 +2,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace IDisposableAnalyzer
@@ -26,29 +27,34 @@ namespace IDisposableAnalyzer
         public override void Initialize(AnalysisContext context)
         {
             // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            //context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-            context.RegisterSyntaxNodeAction(c=> AnalyzeSymbol(c), SyntaxKind.ObjectCreationExpression);
+            //context.RegisterSymbolAction(AnalyzeObjectCreation, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(c=> AnalyzeObjectCreation(c), SyntaxKind.ObjectCreationExpression);
             //Still identifying class declarations and NOT the instantiations
             //Strongly suspect it's the symbol action being registered is of the wrong type
             //Checkout http://stackoverflow.com/questions/33433487/where-can-i-find-what-symbol-types-are-under-different-symbol-kinds-in-roslyn/33435449
         }
 
-        private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        private static void AnalyzeObjectCreation(SyntaxNodeAnalysisContext context)
         {
             //MAKING PROGRESS - PICK UP FROM https://msdn.microsoft.com/en-us/library/mt297717.aspx
 
-            //var symbol = context.Symbol as ITypeSymbol;
+            ObjectCreationExpressionSyntax objectCreation = (ObjectCreationExpressionSyntax) context.Node;
+            //objectCreation.
+            
+            //SymbolInfo symbolInfo = context.SemanticModel.GetDeclaredSymbol()
+
+            //var symbol = symbolInfo.Symbol as ITypeSymbol;
             //if (symbol == null)
             //    return;
-
+            
             //var interfaces = symbol.Interfaces;
 
-            //if (!interfaces.Any(i=>i.Name == "IDisposable"))
+            //if (!interfaces.Any(i => i.Name == "IDisposable"))
             //    return;
 
             //var diagnostic = Diagnostic.Create(Rule, symbol.Locations[0], symbol.Name);
             //context.ReportDiagnostic(diagnostic);
-     
+
         }
     }
 }
